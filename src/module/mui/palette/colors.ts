7ec3f159b61960c6b4fac6d3cfba716f408fbe9f
Jonas <jonathan.gonzalez@gm2dev.com>
2023-02-09 23:09:47 -0300

    feat(colors): add files with all colors to use

--- src/module/mui/palette/colors.ts
+++ src/module/mui/palette/colors.ts
@@ -1,11 +1,13 @@
 export const colors = {
-    primary: {
-        main: '#556cd6',
-    },
-    secondary: {
-        main: '#19857b',
-    },
-    error: {
-        main: '#d32f2f',
-    },
+    primary: '#735AB3',
+    secondary: '#5842FD',
+    error: '#d32f2f',
+    info: '#2196f3',
+    warning: '#ff9800',
+    warningDark: '#f57c00',
+    //brand colors
+    background: '#141420',
+    cardBackground: '#353449',
+    text: '#f3f3f3',
+    textDark: '#c3c3c3',
 }
