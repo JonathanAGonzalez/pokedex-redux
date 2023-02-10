d0ca572173b13864713f8cd731d8f8f940d394dc
Jonas <jonathan.gonzalez@gm2dev.com>
2023-02-09 21:19:43 -0300

    refactor: remove the component app to use the component's routing system in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { theme } from './module/mui/index';
+import { RoutingSystem } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RoutingSystem />
     </ThemeProvider>
   </React.StrictMode>
 );
