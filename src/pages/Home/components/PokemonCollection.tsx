59b58513b8df030a1e1d5b67bb5f6d82d2acfeb2
Jonas <jonathan.gonzalez@gm2dev.com>
2023-02-09 23:42:16 -0300

    feat(PokemonCollection): add new component to show pokemons

---
+++ src/pages/Home/components/PokemonCollection.tsx
@@ -0,0 +1,11 @@
+import { CardPokemon } from '../../../module/core/components';
+
+export const PokemonCollection = (): JSX.Element => {
+  return (
+    <div>
+      <CardPokemon alt="" description="" src="" title="" />
+    </div>
+  );
+};
+
+export default PokemonCollection;
