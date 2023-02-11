fbf4f377913e9c9bc97b7de5ce3d1a13b6edd597
Jonas <jonathan.gonzalez@gm2dev.com>
2023-02-11 03:29:28 -0300

    feat(main): add provider redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { ThemeProvider } from '@mui/material';
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
 import ReactDOM from 'react-dom/client';
 import { theme } from './module/mui/index';
 import { RoutingSystem } from './routes';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RoutingSystem />
+      <Provider store={store}>
+        <RoutingSystem />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
