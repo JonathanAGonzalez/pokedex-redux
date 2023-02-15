73679c02b7a83af6aa612d9a513daa714e03c9f6
Jonas <jonathan.gonzalez@gm2dev.com>
2023-02-15 13:30:03 -0300

    feat(theme): add property component and typography poppins

--- src/module/mui/index.ts
+++ src/module/mui/index.ts
@@ -1,9 +1,14 @@
 import { createTheme } from '@mui/material/styles';
+import { components } from './components';
 import { palette } from './palette';
+
 
 export const theme = createTheme({
     // Use spacing with caution, the default multiplying factor of Mui is 0.5 (factor * 0.5)rem  
-    // this means that the factor will change if you use a component that is not wrapped in the ThemeProvider where this theme is used.  
     spacing: (factor: number) => `${0.25 * factor}rem`,
-    palette
+    palette,
+    components,
+    typography: {
+        fontFamily: "Poppins, sans-serif",
+    }
 });
