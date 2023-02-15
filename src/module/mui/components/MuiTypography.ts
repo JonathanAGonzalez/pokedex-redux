c0e2d2c137a156a2218e5b60e0b75c779c70bf25
Jonas <jonathan.gonzalez@gm2dev.com>
2023-02-15 13:28:20 -0300

    feat(MuiTypography): add new component to change styles in typography

---
+++ src/module/mui/components/MuiTypography.ts
@@ -0,0 +1,9 @@
+import { Components } from "@mui/material";
+
+export const MuiTypography: Components['MuiTypography'] = {
+    styleOverrides: {
+        root: {
+            margin: 0,
+        },
+    }
+}
